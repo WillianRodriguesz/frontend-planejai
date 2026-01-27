e3be6974bdd9f6b35f2a0b1f2c1a591b75462e61
Willian Rodrigues <willianrdriguesz@gmail.com>
2026-01-26 21:29:57 -0300

    fix: viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,15 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  
+  base: "/",
+  
   server: {
     host: true,
     port: 5173,
     proxy: {
       "/planejai": {
-        target: "http://192.168.0.106:3000",
+        target: "http://72.60.52.217:3000", 
         changeOrigin: true,
         secure: false,
         cookieDomainRewrite: "localhost",
